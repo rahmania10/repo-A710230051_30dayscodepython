a9a6a48f1ce7b3fada367182d2dbf5e79d953d8f
rahmania10 <rahmaniamiftahul844@gmail.com>
2024-06-15 10:24:25 +0700

    Add files via upload

---
+++ DAY 3/memanggil method.py
@@ -0,0 +1,25 @@
+class Dog:
+     
+   def __init__(self, nama, umur):
+         
+        
+ 
+    
+        
+ 
+    
+        
+        self.nama = nama
+        self.umur = umur
+   def duduk(self):
+      print(f"{self.nama} sekarang duduk")
+   def berdiri(self):
+      print(f"{self.nama} sekarang berdiri")
+ 
+my_dog = Dog("Wili",6)
+  
+print(f"anjingku bernama {my_dog.nama}")
+print(f"anjingku berumur {my_dog.umur} tahun")
+ 
+my_dog.duduk()
+my_dog.berdiri()
