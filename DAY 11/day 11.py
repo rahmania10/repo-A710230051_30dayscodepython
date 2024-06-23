584982b9833c4a502027151bcf458a5f351a1d1d
rahmania10 <rahmaniamiftahul844@gmail.com>
2024-06-23 16:47:51 +0700

    Add files via upload

---
+++ DAY 11/day 11.py
@@ -0,0 +1,9 @@
+person = {
+    "nama": "Budi",
+    "umur": 30,
+    "pekerjaan": "Guru"
+}
+
+print(person["nama"])  # Mencetak nilai dengan kunci "nama": "Budi"
+person["pekerjaan"] = "Dokter"  # Mengubah nilai dengan kunci "pekerjaan"
+print(person)  
