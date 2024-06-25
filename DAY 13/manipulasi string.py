dde26e30b314b38272a74305523fe916835f0a4f
rahmania10 <rahmaniamiftahul844@gmail.com>
2024-06-25 09:07:03 +0700

    Add files via upload

---
+++ DAY 13/manipulasi string.py
@@ -0,0 +1,6 @@
+sentence = "Belajar Python menyenangkan!"
+
+print(sentence.upper())  # Mencetak "BELAJAR PYTHON MENYENANGKAN!"
+print(sentence.lower())  # Mencetak "belajar python menyenangkan!"
+print(sentence.count("a"))  # Menghitung jumlah huruf "a": 4
+print(sentence[6:12])  # Mencetak substring "Python"
