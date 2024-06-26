dec7b9bba17becf139fc6b90e9bf79d095d5d60e
rahmania10 <rahmaniamiftahul844@gmail.com>
2024-06-26 13:22:17 +0700

    Add files via upload

---
+++ DAY 14/membuat list.py
@@ -0,0 +1,6 @@
+fruits = ["apel", "pisang", "jeruk"]
+
+print(fruits)  # Mencetak seluruh daftar: ["apel", "pisang", "jeruk"]
+print(fruits[1])  # Mencetak item pada indeks 1: "pisang"
+fruits.append("mangga")  # Menambahkan item baru ke akhir daftar
+print(fruits)  # Mencetak daftar yang diperbarui: ["apel", "pisang", "jeruk", "mangga"]
